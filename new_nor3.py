3121b06b51e3010a679d319b8a9c050fd4934657
Progressionn <113501428+Progressionn@users.noreply.github.com>
2023-05-18 21:18:11 +0300

    Add files via upload

--- new_nor3.py
+++ new_nor3.py
@@ -8,8 +8,9 @@ bot = telebot.TeleBot('5643211936:AAEYLgx-T4N0JpyPTZQ26oKvrO2iK48h_-s')
 def rey(message):
     markup = types.InlineKeyboardMarkup()
     button1 = types.InlineKeyboardButton("ИМТ", url='https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D0%B4%D0%B5%D0%BA%D1%81_%D0%BC%D0%B0%D1%81%D1%81%D1%8B_%D1%82%D0%B5%D0%BB%D0%B0')
-    #button2 = types.InlineKeyboardButton("Инструкция", open('1.html','r', encoding='UTF-8').read() )
     markup.add(button1)
+    bot.send_document(message.chat.id, open(r'1.html','rb'))
+    bot.send_sticker(message.chat.id, sticker='CAACAgIAAxkBAAEJBk9kZj94YFypzYQe1Ey4ZBmoXUGyawACwA0AArjWcUp5C0WFjY4Sti8E')
     try:
         file_1 = open('marks.txt', 'r').readlines()[1:]
         file_1 = [int(x) for x in file_1]
@@ -22,33 +23,16 @@ def rey(message):
                          reply_markup=markup)
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Произвести расчет ИМТ")
-    btn2 = types.KeyboardButton("Задать вопрос")
     btn3 = types.KeyboardButton("/help")
     btn4 = types.KeyboardButton("поставить оценку")
     btn5 = types.KeyboardButton("посмотреть отзывы")
     btn6 = types.KeyboardButton("написать отзыв")
-    markup.add(btn1, btn2, btn3, btn4,btn5,btn6)
+    btn7 = types.KeyboardButton("Обзор бота")
+    markup.add(btn1, btn3, btn4, btn5, btn6, btn7)
     bot.send_message(message.chat.id,'Воспользуйтесь моим функционалом, чтобы получить нужную вам информацию.',reply_markup=markup)
 @bot.message_handler(content_types=['text'])
 def get_user_text(message):
-    if (message.text == 'Задать вопрос'):
-            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            btn1 = types.KeyboardButton("Цитаты")
-            btn2 = types.KeyboardButton("Прочее")
-            btn3 = types.KeyboardButton("Вернуться в главное меню")
-            markup.add(btn1, btn2, btn3)
-            bot.send_message(message.chat.id, 'Здесь вы можете получить дополнительные подсказки', reply_markup=markup)
-    elif (message.text == 'Цитаты'):
-        markup = types.InlineKeyboardMarkup()
-        button2 = types.InlineKeyboardButton("Цитаты", url='https://simpleslim.ru/50-luchshih-tsitat-pro-sport-i-zdorove/?ysclid=lfwflm44g1541471881')
-        markup.add(button2)
-        bot.send_message(message.chat.id, 'Здесь вы можете ознакомиться с другими цитатами', reply_markup=markup)
-    elif (message.text == 'Прочее'):
-        markup = types.InlineKeyboardMarkup()
-        button3 = types.InlineKeyboardButton("Мифы об ИМТ", url='https://medaboutme.ru/articles/mify_ob_indekse_massy_tela_imt/?ysclid=lfwigwk8s9152935629')
-        markup.add(button3)
-        bot.send_message(message.chat.id, 'Здесь вы можете ознакомиться с другими фактами об ИМТ', reply_markup=markup)
-    elif (message.text == "Вернуться в главное меню"):
+    if (message.text == "Вернуться в главное меню"):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton("Произвести расчет ИМТ")
         button2 = types.KeyboardButton("Задать вопрос")
@@ -56,7 +40,8 @@ def get_user_text(message):
         button4 = types.KeyboardButton("поставить оценку")
         button5 = types.KeyboardButton("посмотреть оценки")
         button6 = types.KeyboardButton("написать отзыв")
-        markup.add(button1, button2, button3, button4, button5, button6)
+        button7 = types.KeyboardButton("Обзор бота")
+        markup.add(button1, button2, button3, button4, button5, button6, button7)
         bot.send_message(message.chat.id, "Вы вернулись в главное меню", reply_markup=markup)
     elif (message.text == 'поставить оценку'):
         bot.send_message(message.chat.id, 'Введите от одного до пяти')
@@ -67,15 +52,15 @@ def get_user_text(message):
     elif (message.text == 'посмотреть отзывы'):
         file_2 = open('review.txt', 'r').readlines()[1:]
         bot.send_message(message.chat.id, str("\n".join(file_2)))
+    elif (message.text == 'Обзор бота'):
+        bot.send_document(message.chat.id, open(r'1.html', 'rb'))
+        bot.send_document(message.chat.id, 'Здесь вы всегда можете ознакомиться с функционалом бота и обратиться кдополнительным источникам информации')
     elif (message.text == '/help'):
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         button1 = types.KeyboardButton("Функция Произвести расчет ИМТ")
-        button2 = types.KeyboardButton("Функция Задать вопрос")
-        button3 = types.KeyboardButton("Функция Цитаты")
-        button4 = types.KeyboardButton("Функция Прочее")
-        button5 = types.KeyboardButton("Функция Вернуться в главное меню")
-        button6 = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(button1, button2, button3, button4, button5, button6)
+        button2 = types.KeyboardButton("Функция Вернуться в главное меню")
+        button3 = types.KeyboardButton("Вернуться в главное меню")
+        markup.add(button1, button2, button3)
         bot.send_message(message.chat.id, "Здесь вы можете узнать о функционале данного телеграмм-бота.", reply_markup = markup)
     elif (message.text == 'Функция Произвести расчет ИМТ'):
         bot.send_message(message.chat.id, 'Эта функция запрашивает у пользователя его рост и вес и считает его индекс массы тела по следующей формуле - рост / (вес*вес) и округляет его до трех цифр после запятой.')
@@ -109,30 +94,68 @@ def review(message):
     file = open('review.txt', 'a').write('\n' + message.text)
     bot.send_message(message.chat.id, 'Ваш отзыв записан')
 def weightandheight(message):
-    Citates = ['Сила зависит не от физических способностей, а от несгибаемой воли. Махатма Ганди.','Пять составляющих пути к победе – это стойкость, скорость, сила, мастерство и воля. Причем воля – это самое главное! Кен Доэрти.','В здоровом теле — здоровый дух. Ювенал.']
+    Citates = ["Если хочешь быть лучше других, будь готов делать то, что не хотят делать остальные",
+     "Бог дал тело, на многое способное. Вам остается убедить в этом разум",
+     "Если человек встал после падения, это не физика, а характер",
+     "Важна не скорость продвижения. Важно, что продолжаешь идти», «Если не готов поработать, то приготовься к поражению»",
+     "Победители играют, пока не добьются желаемого",
+     "Спорт – не насилие. A положительные эмоции",
+     "Если здоровье – символ свободы, то спорт – символ здоровья",
+     "Некоторые вещи кажутся невозможными, пока сам не попробуешь выполнить",
+     "Любая трудность дается для чего-то",
+     "Если вы стремитесь к цели всем сердцем, то сумеете добиться своего, даже если другие считают это невозможным",
+     "Возможность преодолеть себя —это, без сомнений, ценное свойство спорта",
+     "Спорт – это лекарство от плохого настроения, от депрессий",
+     "Вы должны наблюдать за вашим телом, если вы хотите, чтобы ваш ум работал правильно",
+     "Надо непременно встряхивать себя физически, чтобы быть здоровым нравственно",
+     "Настойчивость может превратить неудачу в выдающееся достижение",
+     "Спорт – это труд, но не все с ним справляются",
+     "Не бойся неудачи. Это путь к успеху ",
+     "Никогда не говори «никогда», потому что ограничения, как и страхи, часто являются всего лишь иллюзией",
+     "Спорт совершенствует физические качества и проверяет моральные",
+     "Чем тяжелее битва, тем слаще победа",
+     "Возраст не помеха. Это ограничение, которое вы накладываете на свой разум",
+     "Способность побеждать самого себя, без сомнения, является самым ценным из всего, что дарит спорт",
+     "Без напряженной работы над собой, без фанатизма ничего путного нельзя добиться ни в спорте, ни где-либо еще",
+     "Не позволяйте тому, что вы не можете сделать, мешать тому, что вы можете сделать",
+     "Независимо от обстоятельств, через которые вы можете пройти, просто преодолевайте их",
+     "Можно мотивировать страхом, а можно мотивировать наградой. Но оба эти метода носят временный характер. Единственная постоянная вещь – это самомотивация",
+     "Тот, кто хочет добиться убедительных побед, обязан пытаться прыгнуть выше головы",
+     "У вас должна быть не только конкурентоспособность, но и способность, независимо от обстоятельств, с которыми вы сталкиваетесь, никогда не сдаваться",
+     "Никогда не сдавайся! Неудача и отказ – только первый шаг к успеху",
+     "Если ты будешь бояться, ты никогда не будешь радоваться жизни. У тебя есть только один шанс, так что ты должен повеселиться",
+     "Беги, когда можешь, ходи, если нужно, ползай, если нужно; просто никогда не сдавайся",
+     "Лучше начать и ошибаться, чем не начать и сожалеть всю жизнь",
+     "Постоянные усилия, а не сила или интеллект, – это ключ к раскрытию нашего потенциала",
+     "В жизни живет лучше тот, кто окреп хорошо физически",
+     "Многие люди терпят неудачу только потому, что сдаются в двух шагах от успеха",
+     "Бездействие порождает беспокойство и страх. Действие — уверенность и смелость. Если ты хочешь победить страх, не сиди дома и не думай об этом. Встань и действуй",
+     "Начните оттуда, где вы сейчас находитесь. Используйте то, что у вас есть, и делайте всё, что можете",
+     "Вы можете быть кем угодно, если вы уделите этому время"]
     try:
         weight, height = re.split(' и ', message.text, maxsplit=1)
         try:
             weight = float(weight)
-            bot.send_message(message.chat.id, 'Вы ввели данные корректно')
             try:
                 height = float(height)
                 r = str(round(weight / (height * height)))
                 message = bot.send_message(message.chat.id, 'Ваш ИМТ равен - ' + r)
                 if (int(r)) in range(18, 26):
-                    bot.send_message(message.chat.id, '1')
+                    bot.send_message(message.chat.id, 'Ваш вес относится к категории "нормальный вес"')
+                    bot.send_message(message.chat.id, random.choice(Citates))
                 elif (int(r)) in range(26, 31):
-                    bot.send_message(message.chat.id, '2')
+                    bot.send_message(message.chat.id, 'Ваш вес относится к категории "изюыточный вес"')
+                    bot.send_message(message.chat.id, random.choice(Citates))
                 elif (int(r)) in range(31, 36):
-                    bot.send_message(message.chat.id, '3')
+                    bot.send_message(message.chat.id, 'Ваш вес относится к категории "ожирение первой степени"')
+                    bot.send_message(message.chat.id, random.choice(Citates))
                 else:
-                    bot.send_message(message.chat.id, '4')
+                    bot.send_message(message.chat.id, 'Ваш вес относится к категории "ожирение второй степени"')
+                    bot.send_message(message.chat.id, random.choice(Citates))
             except Exception:
                 bot.send_message(message.chat.id, 'Вы ввели данные не в правильном формате. Выберите еще раз подходящюю функцию и введите данные повторно.')
         except Exception:
             bot.send_message(message.chat.id, 'Вы ввели данные не в том формате. Выберите еще раз подходящюю функцию и введите данные повторно.')
     except Exception:
         bot.send_message(message.chat.id, 'Вы ввели данные не в том формате. Выберите еще раз подходящюю функцию и введите данные повторно.')
-    bot.send_message(message.chat.id, random.choice(Citates))
-    bot.send_photo(message.chat.id, photo=open(random.choice(txt_files)))
 bot.polling(none_stop = True, interval = 0)
